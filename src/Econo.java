ab2f434c6d2eea8530ba371606eb300f2118a76f
Rafael Roth <rafael2rooth@gmail.com>
2024-08-15 14:54:40 -0300

    econo ta feito (e feio)
    a lógica ta ai mas ta feio, vou mudar o carro e quebrar tudo provavelmente

--- src/Econo.java
+++ src/Econo.java
@@ -3,29 +3,52 @@ public class Econo extends Carro {
     private int controleQuilometragem;
     
     public Econo(String modelo) {
-        super(modelo, TipoCombustivel.GASOLINA, 20, 1000);
+        super(modelo, TipoCombustivel.GASOLINA, 20, 55); // Arrumar a capacidadeTaqnque
         controleQuilometragem = 0;
     }
 
-    // Viagem modificada conforme o enunciado
+    // Viagem modificada da forma lógicamente correta conforme o enunciado mas n faz muito sentido não
     @Override
     public boolean viaja(int distancia) {
         if (verificaSePodeViajar(distancia) >= distancia) {
             if(getMotor().getQuilometragem() + distancia > controleQuilometragem + 5000 && getMotor().getConsumo() > 10)
             {
+                int dif = calculaDiferenca(distancia);
+
+                System.out.println(dif + "   ||  " + distancia);
+                
+                // Percorre a diferenca até o decresimento do consumo
+                getMotor().percorre(distancia - dif);
+                getTanque().gasta(getMotor().combustivelNecessario(distancia - dif));
+
+                // Automenta o consumo
+                aumentaConsumo();
+
+                // Percorre o resto
+                getMotor().percorre(dif);
+                getTanque().gasta(getMotor().combustivelNecessario(dif));
+
                 controleQuilometragem += 5000;
-                getMotor().setConsumo(getMotor().getConsumo() - 1);
+                return true;
             }
             getMotor().percorre(distancia);
             getTanque().gasta(getMotor().combustivelNecessario(distancia));
             return true;
+
         }
         return false;
     }
+    
 
-    // Deletar -- Usando pra testar
-    public void encheTanque()
+    // Aumenta o consumo do motor
+    private void aumentaConsumo()
     {
-        abastece(TipoCombustivel.GASOLINA, 1000);
+        getMotor().setConsumo(getMotor().getConsumo() - 1);
+    }
+
+    // Calculo da diferenca de distância
+    private int calculaDiferenca(int distancia)
+    {
+        return (distancia + getMotor().getQuilometragem() - controleQuilometragem + 5000) - 10000; 
     }
 }
