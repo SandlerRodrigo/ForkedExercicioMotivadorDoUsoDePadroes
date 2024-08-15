5fb98b885a6684c4b29db47605311dfd0bfedff8
Rafael Roth <rafael2rooth@gmail.com>
2024-08-15 11:41:21 -0300

    Classe econo paricalmente concluida
    tem erro na lógica pq ele só contabiliza o consumo no fim da viagem, e se cruza um dos 5000 durante ela ele n faz nada, tem q arrumar isso ai

--- src/Econo.java
+++ src/Econo.java
@@ -1,7 +1,31 @@
 public class Econo extends Carro {
+
+    private int controleQuilometragem;
     
-    public Econo(String modelo, TipoCombustivel tipoCombustivel) {
-        super(modelo, tipoCombustivel, 20, 55);
+    public Econo(String modelo) {
+        super(modelo, TipoCombustivel.GASOLINA, 20, 1000);
+        controleQuilometragem = 0;
     }
-    
+
+    // Viagem modificada conforme o enunciado
+    @Override
+    public boolean viaja(int distancia) {
+        if (verificaSePodeViajar(distancia) >= distancia) {
+            if(motor.getQuilometragem() + distancia > controleQuilometragem + 5000 && motor.getConsumo() > 10)
+            {
+                controleQuilometragem += 5000;
+                motor.setConsumo(motor.getConsumo() - 1);
+            }
+            motor.percorre(distancia);
+            tanque.gasta(motor.combustivelNecessario(distancia));
+            return true;
+        }
+        return false;
+    }
+
+    // Deletar -- Usando pra testar
+    public void encheTanque()
+    {
+        abastece(TipoCombustivel.GASOLINA, 1000);
+    }
 }
